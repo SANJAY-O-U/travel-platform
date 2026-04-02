5887e974b1d8848329def738036bdf5e2d7721b2
Sanjay Upadhyay <sanjayou2005@gmail.com>
2026-04-03 00:54:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -44,6 +44,7 @@ const corsOptions = {
       'http://127.0.0.1:5173',
       'http://localhost:3000',
       'http://127.0.0.1:3000',
+      'https://travel-platform-sandy.vercel.app/',
       process.env.CLIENT_URL,
     ].filter(Boolean);
 
